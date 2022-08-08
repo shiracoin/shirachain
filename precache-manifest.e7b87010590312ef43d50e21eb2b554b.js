41bf2cc096ac78d169dc90487e28545198371e63
shiracoin <shiracrypto@gmail.com>
2022-08-08 03:37:00 -0400

    first update

---
+++ precache-manifest.e7b87010590312ef43d50e21eb2b554b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a9161a7a0b2a44d0133b6043c5cde5",
+    "url": "/shirachain/android-chrome-192x192.png"
+  },
+  {
+    "revision": "22cb396af4c49bcd58ac943471245ea8",
+    "url": "/shirachain/android-chrome-256x256.png"
+  },
+  {
+    "revision": "da7eed2c3c2b75ee4b6a8d88a47b41f5",
+    "url": "/shirachain/apple-touch-icon.png"
+  },
+  {
+    "revision": "6ad81ade016507afbc22",
+    "url": "/shirachain/css/app.947da754.css"
+  },
+  {
+    "revision": "bdca597595f046a257e6c78b0de1f850",
+    "url": "/shirachain/favicon-16x16.png"
+  },
+  {
+    "revision": "108792b9faf56c4a36ca0f2dfad4697a",
+    "url": "/shirachain/favicon-32x32.png"
+  },
+  {
+    "revision": "ace3bcae6ffe6f414dfb88899dab0af3",
+    "url": "/shirachain/index.html"
+  },
+  {
+    "revision": "6d1933505f0e7a118fd3",
+    "url": "/shirachain/js/about.89a8190b.js"
+  },
+  {
+    "revision": "6ad81ade016507afbc22",
+    "url": "/shirachain/js/app.80a7a1c9.js"
+  },
+  {
+    "revision": "02dfb46af4f99b48ca63",
+    "url": "/shirachain/js/chunk-vendors.1ae836a5.js"
+  },
+  {
+    "revision": "fbabc6022c0ad48abf97552ae2265550",
+    "url": "/shirachain/manifest.json"
+  },
+  {
+    "revision": "a4c09c7108f81f19e364e8eaacc9035a",
+    "url": "/shirachain/mstile-150x150.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shirachain/robots.txt"
+  }
+]);
